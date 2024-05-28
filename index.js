94406b36ea74beb2fca7717ec6da59b0eacc7b1b
Aman Soni <amansoni18860@gmail.com>
2024-05-28 20:24:23 +0530

    Dance Web is initialised

---
+++ index.js
@@ -0,0 +1,11 @@
+const express=require("express")
+const port=5000
+const app=express()
+
+app.get("/",(req,res)=>{
+    res.send("Hello")
+})
+
+app.listen(port,()=>{
+    console.log(`Server is listening at http://localhost:${port}`)
+})
